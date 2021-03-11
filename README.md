963f978e8e633ea55208d614c69e29bf097c7fb6
jny <ny030303@naver.com>
2021-03-11 16:19:02 +0900

    green menu 세가지 셈플 업로드 0311

--- README.md
+++ README.md
@@ -7,4 +7,7 @@
   * 정답 : https://github.com/ny030303/implementScreen__class/tree/master/just-answer
 
 * Green복지재단
-  * Green 내코드 : https://github.com/ny030303/implementScreen__class/tree/master/green
+  * Green 내코드 폴더: https://github.com/ny030303/implementScreen__class/tree/master/green
+    * index -> menu (뒷배경x): https://github.com/ny030303/implementScreen__class/blob/master/green/index.html
+    * index -> menu가 뒷배경과 같이나오는 것(뒷배경 호버x): https://github.com/ny030303/implementScreen__class/blob/master/green/index2.html
+    * index -> menu가 뒷배경과 같이나오는 것(뒷배경 호버o): https://github.com/ny030303/implementScreen__class/blob/master/green/index3.html
---
+++ green/app3.js
@@ -0,0 +1,43 @@
+function onPopup() {
+    $(".popup").show("slow");
+}
+
+function hidePopup() {
+    $(".popup").hide("fast");
+}
+
+$(() => {
+    $(".sub-menu").hide();
+    $(".menu-back").hide();
+    $(".popup").hide();
+
+    $(".menu").hover(function(){
+        $(".sub-menu").stop().slideDown();
+        $(".menu-back").stop().slideDown();
+    }, function() {
+        $(".sub-menu").stop().slideUp();
+        $(".menu-back").stop().slideUp();
+    });
+
+    $(".menu-back").hover(function(){
+        $(".sub-menu").stop().slideDown();
+        $(".menu-back").stop().slideDown();
+    }, function() {
+        $(".sub-menu").stop().slideUp();
+        $(".menu-back").stop().slideUp();
+    });
+
+
+    $(".slider img").css("left", "100%");
+    $(".slider img").eq(0).css("left", "0");
+
+    let num = 0;
+
+    setInterval(function() {
+        let next = (num + 1) % 3;
+
+        $(".slider img").eq(num).stop().animate({"left": "-100%"}, 1500);
+        $(".slider img").eq(next).css({"left": "100%"}).stop().animate({"left": "0"}, 1500);
+        num = next;
+    }, 3000);
+})
